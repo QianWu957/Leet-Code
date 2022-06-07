9d23f3ce212d0483f44c82263f9d3687f2cb942a
qwu <qwu@cs.odu.edu>
2022-06-07 02:30:55 -0400

    Second

--- Hashtable/1_Two_Sum.py
+++ Hashtable/1_Two_Sum.py
@@ -8,6 +8,15 @@ class Solution:
                 return [hash_map[diff], idx] 
             else:
                 hash_map[val] = idx
+     # init solution by two pointers
+        # if not nums: return None
+        # slow, fast = 0, 1
+        # while slow < len(nums) and nums[slow] + nums[fast] != target:
+        #     fast +=1
+        #     if fast == len(nums):
+        #         slow +=1
+        #         fast = slow+1
+        # return [slow, fast]
 
 if __name__ == '__main__':
     a = Solution()
