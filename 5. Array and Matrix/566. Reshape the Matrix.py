a229137489fdc9e1001498931e83a2afddabba70
Qian Wu <qianwu957@gmail.com>
2022-06-25 02:57:17 -0400

    init

--- 5. Array and Matrix/566. Reshape the Matrix.py
+++ 5. Array and Matrix/566. Reshape the Matrix.py
@@ -1,5 +1,5 @@
 class Solution:
-    def matrixReshape(self, mat: List[List[int]], r: int, c: int) -> List[List[int]]:
+    def matrixReshape(self, mat, r: int, c: int):
         if not mat: return 0
         if len(mat) * len(mat[0]) != r * c: return mat
 
@@ -7,4 +7,10 @@ class Solution:
         for x in range (r*c):
             res[x//c][x%c] = mat[x//len(mat[0])][x%len(mat[0])] 
         return res
-    
+
+if __name__ == '__main__':
+    a = Solution()
+    n=[[1,2],[3,4]]
+    r=1
+    c=4
+    print(a.matrixReshape(n,r,c))
