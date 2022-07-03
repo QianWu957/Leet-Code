b161c97b48a8555d707bcb815390dc683604ae6e
Qian Wu <qianwu957@gmail.com>
2022-07-03 00:00:21 -0700

    Init

--- 4. Binary Search/34. Find First and Last Position of Element in Sorted Array.py
+++ 4. Binary Search/34. Find First and Last Position of Element in Sorted Array.py
@@ -1,36 +1,58 @@
 class Solution:
-    def searchRange(self, nums, target: int):
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+
+        # flag = 0
+        # res = []
+        # for i in range(len(nums)):
+        #     if nums[i] == target and flag == 0:
+        #         res.append(i)
+        #         res.append(i)
+        #         flag =1
+        #     elif nums[i]== target:
+        #         # res.append(i-1)
+        #         res[-1] = i
+
+        # if len(res)>=1:
+        #     return res
+        # else:
+        #     return [-1,-1]
+        if not nums: return [-1,-1]
         res = []
-        if not nums: return [-1, -1]
-        l, r = 0, len(nums)-1
-        ## 寻找左边界
-        while l <= r:
-            m = l + (r - l) // 2
-            if nums[m] == target:
-                if m == 0 or nums[m-1] != target:
-                    res.append(m)
-                    break
-                else:
-                    r = m -1
-            elif nums[m] > target:
-                r = m - 1
-            else:
-                l = m + 1
-                
-        l, r = 0, len(nums)-1
+        l, r = 0 , len(nums)-1
         while l<=r:
-            m = l + (r - l) // 2
-            if nums[m] == target:
-                if m == len(nums)-1 or nums[m+1] != target:
-                    res.append(m)
+            mid = l+(r-l)//2
+            if nums[mid] == target:
+                if mid == 0 or nums[mid-1] != target:
+                    res.append(mid)
                     break
                 else:
-                    l = m +1
-            elif nums[m] > target:
-                r = m - 1
+                    r = mid -1
+
+            elif nums[mid] > target:
+                r = mid -1
             else:
-                l = m + 1
-        return res if res else [-1,-1]
+                l = mid +1
+        
+        l, r = 0, len(nums)-1
+        while l <= r:
+            mid = l+(r-l)//2
+            if nums[mid] == target:
+                if mid == len(nums)-1 or nums[mid+1]!= target:
+                    res.append(mid)
+                    break
+                else:
+                    l = mid +1
+            elif nums[mid]> target:
+                r = mid -1
+            else:
+                l = mid+1
+        print(res)
+        print(len(res))
+
+        if len(res) >= 1:
+            return res
+        else:
+            return [-1,-1]
 
 if __name__ == '__main__':
     a = Solution()
