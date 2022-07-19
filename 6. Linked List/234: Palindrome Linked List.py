219b676d885971647f4c2b611cbcc0fa1f9ceaf3
Qian Wu <qianwu957@gmail.com>
2022-07-18 23:52:49 -0400

    second

--- 6. Linked List/234: Palindrome Linked List.py
+++ 6. Linked List/234: Palindrome Linked List.py
@@ -5,24 +5,33 @@
 #         self.next = next
 class Solution:
     def isPalindrome(self, head: ListNode) -> bool:
-        # dummy = ListNode()
-        # dummy.next = head
-        # p = head
-
+        #by using array
+        if not head: return True
         nums = []
         while head:
             nums.append(head.val)
             head = head.next
-        # print(nums)
-        # if len(nums) == 1:
-        #     return True
 
         front, back = 0, len(nums)-1
-        
         while front <= back:
             if nums[front] != nums[back]:
                 return False
             else:
                 front+=1
                 back-=1
+        return True
+
+        #by using stack
+        if not head: return True
+        s = []
+        p = head
+        while p:
+            s.append(p.val)
+            p = p.next
+        # print(s)
+
+        while head:
+            if head.val != s.pop():
+                return False
+            head = head.next
         return True
