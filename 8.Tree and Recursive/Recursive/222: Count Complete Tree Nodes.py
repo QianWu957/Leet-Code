5535ee4bd79e74416ac8dff628e5c0c69456a136
Qian Wu <qianwu957@gmail.com>
2022-08-13 16:26:52 -0400

    init

---
+++ 8.Tree and Recursive/Recursive/222: Count Complete Tree Nodes.py
@@ -0,0 +1,37 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def countNodes(self, root: Optional[TreeNode]) -> int:
+        if not root: return 0
+        left_root = root
+        right_root = root
+        left_depth = 0
+        right_depth = 0
+
+        while left_root:
+            left_root = left_root.left
+            left_depth +=1
+        while right_root:
+            right_root = right_root.right
+            right_depth +=1
+        if left_depth ==  right_depth:
+            return 2**left_depth-1
+
+        else:
+            return self.countNodes(root.left)+self.countNodes(root.right) +1 
+
+
+
+    # def __init__(self):
+    #     self.res = 0
+    # def countNodes(self, root: Optional[TreeNode]) -> int:
+    #     if not root: return 0
+    #     self.res +=1
+    #     self.countNodes(root.left)
+    #     self.countNodes(root.right)
+    #     return self.res
+
