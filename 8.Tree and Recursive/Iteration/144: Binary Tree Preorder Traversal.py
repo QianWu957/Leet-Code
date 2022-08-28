c3231cae4d42197f68a39b740e627ac11211ea70
Qian Wu <qianwu957@gmail.com>
2022-08-28 18:16:39 -0400

    简单题，我再次重拳出击

--- 8.Tree and Recursive/Iteration/144: Binary Tree Preorder Traversal.py
+++ 8.Tree and Recursive/Iteration/144: Binary Tree Preorder Traversal.py
@@ -8,12 +8,23 @@ class Solution:
     def __init__(self):
         self.res = []
     def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        from collections import deque
         if not root: return []
 
-        self.res.append(root.val)
+        queue = deque([root])
+        while queue:
+            node = queue.popleft()
+            self.res.append(node.val)
+            if node.right:
+                queue.insert(0, node.right)
 
-        self.preorderTraversal(root.left)
-        self.preorderTraversal(root.right)
-
+            if node.left:
+                queue.insert(0, node.left)
         return self.res
+
+        # self.res.append(root.val)
+        # self.preorderTraversal(root.left)
+        # self.preorderTraversal(root.right)
+
+        # return self.res
 
