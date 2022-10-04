48b51667a14b19d8bb6e19ec89d70e43519e6b6f
Qian Wu <qianwu957@gmail.com>
2022-10-03 21:24:30 -0400

    init

--- 11. Dynamic Programming/64: Minimum Path Sum.py
+++ 11. Dynamic Programming/64: Minimum Path Sum.py
@@ -6,12 +6,12 @@ class Solution:
         print(dp)
         dp[0][0] = grid[0][0]
 
+        for i in range(1, len(grid)):
+            dp[i][0] = dp[i-1][0] + grid[i][0]
+
         for j in range(1, len(grid[0])):
             dp[0][j] = dp[0][j-1]+ grid[0][j]
 
-        for i in range(1, len(grid)):
-            dp[i][0] = dp[i-1][0] + grid[i][0]
-        
         for i in range(1, len(grid)):
             for j in range(1,len(grid[0])):
                 dp[i][j] = min(dp[i-1][j], dp[i][j-1]) + grid[i][j]
