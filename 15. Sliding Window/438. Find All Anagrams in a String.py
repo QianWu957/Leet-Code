480b67f326662fa95caf113de3a75e53f8906f2c
Qian Wu <qianwu957@gmail.com>
2022-10-26 18:13:01 -0400

    init

---
+++ 15. Sliding Window/438. Find All Anagrams in a String.py
@@ -0,0 +1,37 @@
+class Solution:
+    def findAnagrams(self, s: str, p: str) -> List[int]:
+        if len(p) > len(s):
+            return []
+        
+        # left, right = 0, 0 
+        need = len(p)
+        res = []
+
+        cnt = collections.Counter(p)
+        
+        for right in range(len(s)):
+            ch = s[right]
+            if ch in cnt:
+                if cnt[ch] > 0:
+                    need -=1
+                cnt[ch]-=1
+
+
+            left = right - len(p)
+            if left >=0:
+                ch = s[left]
+                if ch in cnt:
+                    if cnt[ch] >=0:
+                        need +=1
+                    cnt[ch] +=1
+                
+            if need ==0:
+                res.append(right - len(p) +1)
+
+
+            
+        return res
+
+            
+
+               
